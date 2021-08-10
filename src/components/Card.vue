a6ce70a7c7160a9fbcc555f0c56f2e0f89c5dfc9
Jinnie Cho <jinniethecho@gmail.com>
2021-08-10 13:25:45 +0900

    💄 Created Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="product-card">
+      <img :src="product.image" alt="product-card-1" class="product-card-image" />
+      <h4>{{ product.title }}</h4>
+      <p>€{{ product.price }}</p>
+    <!-- <div class="like-count">
+      <button @click="likes[0] ++">Likes</button>
+      <span>{{likes[0]}}</span>
+    </div> -->  
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props : {
+        product: Object,
+    },
+}
+</script>
+
+<style>
+.product-card {
+    max-width: 640px;
+    height: auto;
+    margin-bottom: 120px;
+}
+
+.product-card h4{
+    margin-bottom: 20px;
+}
+
+.product-card-image {
+    width: 100%;
+    margin-bottom: 42px;
+    border-radius: 4px;
+}
+
+@media screen and (max-width: 640px) {
+   .product-card-image {
+        border-radius: 0;
+}
+}
+</style>
