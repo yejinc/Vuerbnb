6508f62d5d5d5b9604fad232edc0ae8a9b31460c
Jinnie Cho <jinniethecho@gmail.com>
2021-08-10 13:25:29 +0900

    🗃 Added products data

---
+++ src/assets/vuerbnb.js
@@ -0,0 +1,48 @@
+export default [{
+    id : 0,
+    title: "Modern Loft with two terrasses in Friedrichshain",
+    image: "https://a0.muscache.com/im/pictures/miso/Hosting-50515035/original/8ff74aba-6686-4835-9852-54e483a572f2.jpeg?im_w=960",
+    rentalHost: "Entire loft hosted by Martin",
+    content: "2 guests · 1 bedroom · 1 bed · 1 bathroom",
+    price: 344
+    },
+    {
+    id : 1,
+    title: "Bright & Cosy Room @ Checkpoint Charlie",
+    image: "https://a0.muscache.com/im/pictures/63b2ce9d-3070-45ab-87cf-88ac5137771e.jpg?im_w=960",
+    rentalHost: "Private room in rental unit hosted by Lucjan",
+    content: "2 guests · 1 bedroom · 1 bed · 2 shared bathrooms",
+    price: 187
+    },
+    {
+    id : 2,
+    title: "Pretty apartment close to metro in Steglitz Berlín",
+    image: "https://a0.muscache.com/im/pictures/118cd6ce-0043-4266-a83a-99c5c123c653.jpg?im_w=960",
+    rentalHost: "Entire rental unit hosted by Andrea",
+    content: "2 guests · 1 bedroom · 1 bed · 1 bathroom",
+    price: 161
+    },
+    {
+    id : 3,
+    title: "Dachterrassenwohnung 200m vom Ku´Damm entfernt 2",
+    image: "https://a0.muscache.com/im/pictures/89896e6a-cd84-41f5-ae1c-71a6b297408c.jpg?im_w=1200",
+    rentalHost: "Entire rental unit hosted by Antje",
+    content: "2 guests · 1 bedroom · 1 bed · 1 bathroom",
+    price: 299
+    },
+    {
+    id : 4,
+    title: "Central Tranquility",
+    image: "https://a0.muscache.com/im/pictures/6a57fe73-a2c2-4eea-88bf-58a0a4917863.jpg?im_w=1200",
+    rentalHost: "Entire apartment hosted by Dagmar",
+    content: "2 guests · 1 bedroom · 0 beds · 1 bathroom",
+    price: 192
+    },
+    {
+    id : 5,
+    title: "Huge & Cozy Room in the Heart of Friedrichshain",
+    image: "https://a0.muscache.com/im/pictures/7f0fd8e9-2df2-45f7-a67a-15eee54ad7a7.jpg?im_w=960",
+    rentalHost: "Private room in rental unit hosted by Robert",
+    content: "2 guests · 1 bedroom · 1 bed · 1 shared bathroom",
+    price: 298
+  }];
